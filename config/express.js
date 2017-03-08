ab466c90d674d3e11ab9bb7ce0eb81d4b9e5dcac
ovais <ovais@mobinspire.com>
2017-03-08 10:22:45 +0500

    updates

--- config/express.js
+++ config/express.js
@@ -24,6 +24,14 @@ module.exports = function(app, config) {
   }));
   app.use(cookieParser());
   app.use(compress());
+
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
   app.use(express.static(config.root + '/public'));
   app.use(methodOverride());
 
